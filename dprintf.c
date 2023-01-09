129cca678244d614a7f6ea98ce498fa2b9193b8f
Martin Leroy <martin.leroy@ecole-89.com>
2023-01-09 12:04:55 +0100

    save 2

---
+++ dprintf.c
@@ -0,0 +1,43 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     printf
+ * created on:  2023-01-09 - 11:20 +0100
+ * 1st author:  martin.leroy - martin.leroy
+ * description: printf function
+ */
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int stu_print_base10(int nb);
+
+int puts_plus(int channel, const char *str, int rt);
+
+int stu_dprintf(const char *pattern, ...)
+{
+    va_list la;
+    int n;
+
+    n = 0;
+    va_start(la, pattern);
+    while (pattern[n] != '\0') {
+        if (pattern[n] == '%' && pattern[n + 1] == 's') {
+            puts_plus(1, va_arg(la, char *), 0);
+            n = n + 1;
+        } else if (pattern[n] == '%' && pattern[n + 1] == '%') {
+            puts_plus(1, "%", 0);
+            n = n + 1;
+        } else if (pattern[n] == '%' && pattern[n + 1] == 'd'){
+            stu_print_base10(va_arg(la, int));
+            n = n + 1;
+        } else if (pattern[n] == '\n') {
+            write(1, "\n", 1);
+        } else {
+            write(1, &pattern[n], 1);
+        }
+        n = n + 1;
+    }
+    va_end(la);
+    return (n);
+}
